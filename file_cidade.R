1de617e0eba25acf796011a9b84c378777b1b0d0
joaobtj <joao.tolentino@ufsc.br>
2022-05-14 16:33:11 -0300

    update

--- file_cidade.R
+++ file_cidade.R
@@ -1,7 +1,6 @@
-file_cidade <- function(cidade){
-  
+file_tempo <- function(cidade) {
   obj <- readxl::read_excel("estacoes.xlsx", sheet = "SC") %>% dplyr::filter(DC_NOME == cidade)
-  
+
   anos <- with(
     obj,
     seq(
@@ -10,19 +9,27 @@ file_cidade <- function(cidade){
       1
     )
   )
-  
+
+  return(list(anos=anos, 
+              obj=obj))
+}
+
+file_cidade <- function(cidade) {
+ tempo <-  file_tempo(cidade)
+ 
+
   a <- list()
-  for (i in seq_along(anos[1])) {
-    a[[i]] <- with(obj, get_inmet(
-      data_ini = paste0(anos[i], "-01-01"),
-      data_fim = paste0(anos[i] + 1, "-01-01"),
+  for (i in seq_along(tempo$anos)) {
+    a[[i]] <- with(tempo$obj, get_inmet(
+      data_ini = paste0(tempo$anos[i], "-01-01"),
+      data_fim = paste0(tempo$anos[i] + 1, "-01-01"),
       station = CD_ESTACAO,
       alt = VL_ALTITUDE,
       lat = VL_LATITUDE
-    )) %>% filter(year(data) == anos[i])
+    )) %>% filter(year(data) == tempo$anos[i])
   }
-  
-  readr::write_csv(bind_rows(a), paste0("data/",cidade,".csv"))
-  
-  return(obj)
+
+  readr::write_csv(bind_rows(a), paste0("data/", cidade, ".csv"))
+
+
 }
